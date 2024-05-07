4decf552b9581fb8b02984ab4f9ff3ef01283c10
Naziur <robi.ict20@gmail.com>
2024-05-07 12:11:08 +0600

    calculate sum

---
+++ main.js
@@ -0,0 +1,16 @@
+
+ function main(x, y) {
+if (typeof x !== 'number') {
+    return "Please input a valid number"
+
+}
+// console.log(typeof y !== 'number')
+if (typeof y !== 'number') {
+    return "Please input a valid number"
+
+}
+ return Number(x) + Number(y)
+ }
+ const result = main(100, 1132465415451)
+ console.log(result)
+
